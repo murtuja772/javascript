0a77cc071ace5018a98dfd4aa2079bb2a68a876a
murtuja772 <23bcs13248@cuchd.in>
2025-08-21 12:22:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const textArea = document.getElementById("textArea");
 const charCount = document.getElementById("charCount");
 
-// Event listener for typing
 textArea.addEventListener("input", () => {
-  charCount.textContent = textArea.value.length;
+  // Remove spaces before counting
+  const textWithoutSpaces = textArea.value.replace(/\s/g, "");
+  charCount.textContent = textWithoutSpaces.length;
 });
+
